4e63e926b37b571b3cd8d7875a56ffd96a4340fc
checho <chechoz20@gmail.com>
2023-07-23 13:03:46 -0500

    initial commit

---
+++ HolaMundoApp/HolaMundoApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using HolaMundoApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace HolaMundoApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        private string _username;
+        private string _password;
+
+        public string UserName { get => _username; set => SetProperty(ref _username, value); }
+        public string Password { get => _password; set => SetProperty(ref _password, value); }
+
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            if (ValidateFields())
+            {
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
+        }
+        private bool ValidateFields()
+        {
+            return !string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password);
+        }
+    }
+}
---
+++ HolaMundoApp/HolaMundoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HolaMundoApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace HolaMundoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
