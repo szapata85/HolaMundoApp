8b0044587a74afd5daf0e5daa3ef66759523a374
checho <chechoz20@gmail.com>
2023-08-02 23:37:40 -0500

    change File Class Client to Clients

---
+++ HolaMundoApp/HolaMundoApp/Views/ClientsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolaMundoApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClientsPage : ContentPage
+    {
+        public ClientsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
