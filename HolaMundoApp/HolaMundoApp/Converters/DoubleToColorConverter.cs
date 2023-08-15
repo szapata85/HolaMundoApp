f2adf37f84aefede6e3225825acc444edbf928fd
checho <chechoz20@gmail.com>
2023-08-15 00:46:05 -0500

    create converter and apply style resource into ClientPage

---
+++ HolaMundoApp/HolaMundoApp/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace HolaMundoApp.Converters
+{
+    public class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Double doubleValue))
+            {
+                return Color.Black;
+            }
+
+            if (doubleValue > 80)
+            {
+                return Color.Green;
+            }
+            else if (doubleValue > 50)
+            {
+                return Color.Yellow;
+            }
+            else if (doubleValue > 30)
+            {
+                return Color.Orange;
+            }
+            else
+            {
+                return Color.Red;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
