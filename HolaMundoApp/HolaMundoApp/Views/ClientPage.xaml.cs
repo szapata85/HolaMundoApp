e6c1976090f3d6ca9b01f8c6ff2053fa7961be5d
checho <chechoz20@gmail.com>
2023-08-14 21:37:52 -0500

    Add Maps Components in Client Page

--- HolaMundoApp/HolaMundoApp/Views/ClientPage.xaml.cs
+++ HolaMundoApp/HolaMundoApp/Views/ClientPage.xaml.cs
@@ -1,20 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using HolaMundoApp.ViewModels;
+using Xamarin.Forms.Maps;
+using System.ComponentModel;
 
 namespace HolaMundoApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ClientPage : ContentPage
     {
+        private readonly ClientViewModel _viewModel;
         public ClientPage()
         {
             InitializeComponent();
+            _viewModel = Startup.Resolve<ClientViewModel>();
+            BindingContext = _viewModel;
+            _viewModel.PropertyChanged += _viewModel_PropertyChanged;
+        }
+
+        private void _viewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(ClientViewModel.Client) &&
+                _viewModel.Client.Latitude != default &&
+                _viewModel.Client.Longitude != default &&
+                !string.IsNullOrEmpty(_viewModel.Client.Name))
+            {
+                Position position = new Position(_viewModel.Client.Latitude, _viewModel.Client.Longitude);
+                ClientLocationMap.MoveToRegion(new MapSpan(position, 0.01, 0.01));
+                ClientLocationMap.Pins.Add(new Pin
+                {
+                    Label = _viewModel.Client.Name,
+                    Position = position
+                });
+
+            }
+
         }
     }
 }
