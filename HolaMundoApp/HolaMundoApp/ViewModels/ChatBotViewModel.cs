dbf07216cb05b794f062b7271d3330cc35abac94
checho <chechoz20@gmail.com>
2023-08-18 15:51:05 -0500

    create other page and view model for chatbot

---
+++ HolaMundoApp/HolaMundoApp/ViewModels/ChatBotViewModel.cs
@@ -0,0 +1,68 @@
+﻿using HolaMundoApp.Data.Models;
+using HolaMundoApp.Resources;
+using HolaMundoApp.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace HolaMundoApp.ViewModels
+{
+    public class ChatBotViewModel : BaseViewModel
+    {
+        private readonly IChatService _chatService;
+        public ICommand AppearingCommand { get; set; }
+        public ICommand SendMsgCommand { get; }
+
+        
+        private string _Username;
+        public string Username { get => _Username; set => SetProperty(ref _Username, value); }
+
+        private string _MessageIn;
+        public string MessageIn { get => _MessageIn; set => SetProperty(ref _MessageIn, value); }
+
+        private IEnumerable<MessageModel> _MessagesList;
+        public IEnumerable<MessageModel> MessagesList { get => _MessagesList; set => SetProperty(ref _MessagesList, value); }
+
+        public ChatBotViewModel(IChatService chatService)
+        {
+            _chatService = chatService;
+            AppearingCommand = new AsyncCommand(async () => await OnAppearingAsync());
+            SendMsgCommand = new AsyncCommand(async () => await OnSendMsgClicked());
+
+            _chatService.Connect();
+            _chatService.ReceiveMessage(GetMessage);
+        }
+
+        private async Task OnSendMsgClicked()
+        {
+            if (!string.IsNullOrEmpty(MessageIn))
+            {
+                await _chatService.SendMessageToAll(new MessageItem { Message = MessageIn, SourceId = Username });
+                AddMessage(Username, MessageIn, true);
+            }
+        }
+
+        private void GetMessage(MessageItem messageItem)
+        {
+            if (messageItem.SourceId != Username && !string.IsNullOrEmpty(messageItem.Message))
+                AddMessage(messageItem.SourceId, messageItem.Message, false);
+        }
+
+            private void AddMessage(string User, string message, bool isOwner = true)
+        {
+            List<MessageModel> tempList = MessagesList.ToList();
+            tempList.Add(new MessageModel { IsOwnerMessage = isOwner, Message = message, UserName = User });
+            MessagesList = new List<MessageModel>(tempList);
+            MessageIn = string.Empty;
+        }
+
+        private async Task OnAppearingAsync()
+        {
+            Username = GlobalVarsApplication.USERNAME;
+            MessagesList = new List<MessageModel>();
+            await _chatService.Init(Username);
+        }
+    }
+}
---
+++ HolaMundoApp/HolaMundoApp/Views/ChatBotPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HolaMundoApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ChatBotPage : ContentPage
+	{
+		public ChatBotPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
