263f73b0ed6a6851e446a399dcf14beabe2569e5
checho <chechoz20@gmail.com>
2023-08-18 14:41:39 -0500

    hotfix style grid Message

--- HolaMundoApp/HolaMundoApp/ViewModels/ChatRoomViewModel.cs
+++ HolaMundoApp/HolaMundoApp/ViewModels/ChatRoomViewModel.cs
@@ -17,12 +17,14 @@ namespace HolaMundoApp.ViewModels
         public ICommand AppearingCommand { get; set; }
         public ICommand SendMsgCommand { get; }
 
+        
         private string _Username;
-        private string _Message;
-        private IEnumerable<MessageModel> _MessagesList;
         public string Username { get => _Username; set => SetProperty(ref _Username, value); }
-        public string Message { get => _Message; set => SetProperty(ref _Message, value); }
 
+        private string _MessageIn;
+        public string MessageIn { get => _MessageIn; set => SetProperty(ref _MessageIn, value); }
+
+        private IEnumerable<MessageModel> _MessagesList;
         public IEnumerable<MessageModel> MessagesList { get => _MessagesList; set => SetProperty(ref _MessagesList, value); }
 
         public ChatRoomViewModel(IChatService chatService)
@@ -30,20 +32,21 @@ namespace HolaMundoApp.ViewModels
             _chatService = chatService;
             AppearingCommand = new AsyncCommand(async () => await OnAppearingAsync());
             SendMsgCommand = new AsyncCommand(async () => await OnSendMsgClicked());
-            //SendMsgCommand = new Command(OnSendMsgClicked);
 
             _chatService.Connect();
+            _chatService.ReceiveMessage(GetMessage);
         }
 
         private async Task OnSendMsgClicked()
         {
-            await _chatService.SendMessageToAll(new MessageItem { Message = Message, SourceId = Username});
-            AddMessage(Username, Message, true);
+            await _chatService.SendMessageToAll(new MessageItem { Message = MessageIn, SourceId = Username});
+            AddMessage(Username, MessageIn, true);
         }
 
         private void GetMessage(MessageItem messageItem)
         {
-            AddMessage(messageItem.SourceId, messageItem.Message, false);
+            if (messageItem.SourceId != Username)            
+                AddMessage(messageItem.SourceId, messageItem.Message, false);
         }
 
             private void AddMessage(string User, string message, bool isOwner = true)
@@ -51,14 +54,13 @@ namespace HolaMundoApp.ViewModels
             List<MessageModel> tempList = MessagesList.ToList();
             tempList.Add(new MessageModel { IsOwnerMessage = isOwner, Message = message, UserName = User });
             MessagesList = new List<MessageModel>(tempList);
-            Message = string.Empty;
+            MessageIn = string.Empty;
         }
 
         private async Task OnAppearingAsync()
         {
             Username = GlobalVarsApplication.USERNAME;
             MessagesList = new List<MessageModel>();
-            _chatService.ReceiveMessage(GetMessage);
             await _chatService.Init(Username);
         }
     }
