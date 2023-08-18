2986e46f34c0f73676fb942170f65d78dfcdcb53
checho <chechoz20@gmail.com>
2023-08-17 22:26:50 -0500

    Connect server Signal R

---
+++ HolaMundoApp/HolaMundoApp/Data/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HolaMundoApp.Data.Models
+{
+    public class MessageModel
+    {
+        public long ID { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+        public bool IdOwnerMessage { get; set; }
+    }
+}
--- HolaMundoApp/HolaMundoApp/ViewModels/ChatRoomViewModel.cs
+++ HolaMundoApp/HolaMundoApp/ViewModels/ChatRoomViewModel.cs
@@ -2,6 +2,8 @@
 using HolaMundoApp.Resources;
 using HolaMundoApp.Services;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.CommunityToolkit.ObjectModel;
@@ -14,20 +16,49 @@ namespace HolaMundoApp.ViewModels
     {
         private readonly IChatService _chatService;
         public ICommand AppearingCommand { get; set; }
+        public ICommand SendMsgCommand { get; }
 
         private string _Username;
-
+        private string _Message;
+        private IEnumerable<MessageModel> _MessagesList;
         public string Username { get => _Username; set => SetProperty(ref _Username, value); }
+        public string Message { get => _Message; set => SetProperty(ref _Message, value); }
+
+        public IEnumerable<MessageModel> MessagesList { get => _MessagesList; set => SetProperty(ref _MessagesList, value); }
 
         public ChatRoomViewModel(IChatService chatService)
         {
             _chatService = chatService;
-            this.AppearingCommand = new AsyncCommand(async () => await OnAppearingAsync());
+            AppearingCommand = new AsyncCommand(async () => await OnAppearingAsync());
+            SendMsgCommand = new AsyncCommand(async () => await OnSendMsgClicked());
+            //SendMsgCommand = new Command(OnSendMsgClicked);
+        }
+
+        private async Task OnSendMsgClicked()
+        {
+            await _chatService.SendMessageToAll(new MessageItem { Message = Message, SourceId = 0});
+            AddMessage(Username, Message, true);
+        }
+
+        private void GetMessage(MessageItem messageItem)
+        {
+            AddMessage(messageItem.SourceId.ToString(), messageItem.Message, false);
+        }
+
+            private void AddMessage(string User, string message, bool isOwner = true)
+        {
+            List<MessageModel> tempList = MessagesList.ToList();
+            tempList.Add(new MessageModel { IdOwnerMessage = isOwner, Message = message, UserName = User });
+            MessagesList = new List<MessageModel>(tempList);
+            Message = string.Empty;
         }
 
         private async Task OnAppearingAsync()
         {
             Username = GlobalVarsApplication.USERNAME;
+
+            _chatService.ReceiveMessage(GetMessage);
+            await _chatService.Connect();
         }
     }
 }
