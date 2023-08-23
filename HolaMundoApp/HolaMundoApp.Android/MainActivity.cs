1952d1bf555108c3fd2ebd6f163f5edb5ce79ca9
checho <chechoz20@gmail.com>
2023-08-22 21:02:29 -0500

    Change Icon Desktop

--- HolaMundoApp/HolaMundoApp.Android/MainActivity.cs
+++ HolaMundoApp/HolaMundoApp.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android;
 
 namespace HolaMundoApp.Droid
 {
-    [Activity(Label = "HolaMundoApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "CFABot", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         const int RequestLocationId = 0;
