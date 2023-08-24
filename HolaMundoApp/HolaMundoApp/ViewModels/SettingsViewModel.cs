65c5f03eba9c63900819555b7b71718136b9a82d
checho <chechoz20@gmail.com>
2023-08-23 23:56:44 -0500

    Add command to swith xamarin forms

--- HolaMundoApp/HolaMundoApp/ViewModels/SettingsViewModel.cs
+++ HolaMundoApp/HolaMundoApp/ViewModels/SettingsViewModel.cs
@@ -1,9 +1,24 @@
-﻿namespace HolaMundoApp.ViewModels
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace HolaMundoApp.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
+        public ICommand swNotifyCommand { get; set; }
+
+        private bool _IsToggled;
+        public bool IsToggled { get => _IsToggled; set => SetProperty(ref _IsToggled, value); }
+
         public SettingsViewModel()
         {
+            swNotifyCommand = new AsyncCommand<bool>(async (value) => await onNotifiCommand(value));
+        }
+
+        private async Task onNotifiCommand(bool value)
+        {
+            
         }
     }
 }
